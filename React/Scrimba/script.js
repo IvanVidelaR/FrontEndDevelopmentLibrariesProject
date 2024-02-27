1bd74e523e2597a80199f5cc54217fd172f0e7c2
VidelaR <videlaivan10@gmail.com>
2024-02-26 21:13:51 -0300

    Building a React info site Lesson 1 Scrimba

---
+++ React/Scrimba/script.js
@@ -0,0 +1,3 @@
+
+ReactDOM.render(<h1>Hello everyone!</h1>, document.getElementById("root")); //It allows us to render something to the screen.
+ReactDOM.render(<p>Hola, como estás</p>, document.getElementById("root"));
