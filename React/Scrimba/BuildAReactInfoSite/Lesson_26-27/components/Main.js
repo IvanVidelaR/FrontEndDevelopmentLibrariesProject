8079cc5af7355db8daefc3cfa371f4f2ffa42213
VidelaR <videlaivan10@gmail.com>
2024-03-06 10:55:24 -0300

    Building a React info site Lesson 27 Scrimba

---
+++ React/Scrimba/BuildAReactInfoSite/Lesson_26-27/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main-title">Fun facts about React</h1>
+            <ul className="main-list">
+                <li className="main-list-bullet">Was first released in 2013</li>
+                <li className="main-list-bullet">Was originally created by Jordan Walke</li>
+                <li className="main-list-bullet">Has well over 100K stars on GitHub</li>
+                <li className="main-list-bullet">Is maintained by Facebook</li>
+                <li className="main-list-bullet">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
