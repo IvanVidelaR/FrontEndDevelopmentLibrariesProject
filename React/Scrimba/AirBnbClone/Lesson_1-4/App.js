4225f5d176e13b74a92664832e0a5684fd385bd1
VidelaR <videlaivan10@gmail.com>
2024-03-11 17:09:59 -0300

    Building a AirBnb Clone Lesson 3-4 Scrimba

--- React/Scrimba/AirBnbClone/Lesson_1-4/App.js
+++ React/Scrimba/AirBnbClone/Lesson_1-4/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 /*
+Lesson 3:
 Challenge: Build the Hero component.
 Check the Figma file for the design specifics.
 */
