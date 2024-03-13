07745c3ed5461e98b7700ef637b8d59bb574faee
VidelaR <videlaivan10@gmail.com>
2024-03-13 16:00:58 -0300

    Building a AirBnb Clone Modificacines proyecto airbnb Scrimba

--- React/Scrimba/AirBnbClone/Lesson_1-4/App.js
+++ React/Scrimba/AirBnbClone/Lesson_1-4/App.js
@@ -1,17 +1,21 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-/*
-Lesson 3:
-Challenge: Build the Hero component.
-Check the Figma file for the design specifics.
-*/
+import Card from "./components/Card"
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
