ffbcb8489ab77b69c90ef22f95cc0397a6621741
VidelaR <videlaivan10@gmail.com>
2024-03-13 16:00:34 -0300

    Building a AirBnb Clone Lesson 18-19 Scrimba

--- React/Scrimba/AirBnbClone/Lesson_13-15_Destructuring_Practice/App.js
+++ React/Scrimba/AirBnbClone/Lesson_13-15_Destructuring_Practice/App.js
@@ -1,6 +1,21 @@
 import React from "react"
 import Joke from "./Joke"
 
+
+
+/* Lesson 19: */
+export default function App() {
+    const jokeElements = jokesData.map(joke => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
+    return (
+        <div>
+            {jokeElements}
+        </div>
+    )
+}
+
+
 /*
 Challenge: Think critically - how would you pass in a prop that wasn't
 a string datatype.
@@ -10,41 +25,37 @@ prop that is a number, as well as a prop with an array of comments, and a boolea
 of whether the joke is a pun (`isPun`).
 */
 
-export default function App() {
-    return (
-        <div>
-            <Joke 
-                punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-                isPun={true}
-                upvotes={10}
-                downvotes={2}
-                comments={[{author: "", body: "", title: ""}]}
-            />
-            <Joke 
-                setup="I got my daughter a fridge for her birthday." 
-                punchline="I can't wait to see her face light up when she opens it." 
-                isPun={false}
-            />
-            <Joke 
-                setup="How did the hacker escape the police?" 
-                punchline="He just ransomware!"
-                isPun={true}
-            />
-            <Joke 
-                setup="Why don't pirates travel on mountain roads?" 
-                punchline="Scurvy." 
-                isPun={true}
-            />
-            <Joke 
-                setup="Why do bees stay in the hive in the winter?" 
-                punchline="Swarm." 
-                isPun={true}
-            />
-            <Joke 
-                setup="What's the best thing about Switzerland?" 
-                punchline="I don't know, but the flag is a big plus!" 
-                isPun={false}
-            />
-        </div>
-    )
-}
+/* Lesson 15:
+    <Joke 
+        punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+        isPun={true}
+        upvotes={10}
+        downvotes={2}
+        comments={[{author: "", body: "", title: ""}]}
+    />
+    <Joke 
+        setup="I got my daughter a fridge for her birthday." 
+        punchline="I can't wait to see her face light up when she opens it." 
+        isPun={false}
+    />
+    <Joke 
+        setup="How did the hacker escape the police?" 
+        punchline="He just ransomware!"
+        isPun={true}
+    />
+    <Joke 
+        setup="Why don't pirates travel on mountain roads?" 
+        punchline="Scurvy." 
+        isPun={true}
+    />
+    <Joke 
+        setup="Why do bees stay in the hive in the winter?" 
+        punchline="Swarm." 
+        isPun={true}
+    />
+    <Joke 
+        setup="What's the best thing about Switzerland?" 
+        punchline="I don't know, but the flag is a big plus!" 
+        isPun={false}
+    />
+*/ 
