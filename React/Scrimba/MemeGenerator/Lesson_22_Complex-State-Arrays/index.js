f915be9d9f8d9b234b5212a014cd0f8ded930962
VidelaR <videlaivan10@gmail.com>
2024-03-17 11:27:09 -0300

    Building a Meme Generator Lesson 21-22 State Arrays Scrimba

---
+++ React/Scrimba/MemeGenerator/Lesson_22_Complex-State-Arrays/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    /**
+     * Challenge: See if you can do it all again by yourself :)
+     */
+    const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"]);
+    
+    function addItem() {
+        setThingsArray(prevThingsArray => {
+            return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`]
+        })
+    }
+    
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>);
+    
+    return (
+        <div>
+            <button onClick={addItem}>Add Item</button>
+            {thingsElements}
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
