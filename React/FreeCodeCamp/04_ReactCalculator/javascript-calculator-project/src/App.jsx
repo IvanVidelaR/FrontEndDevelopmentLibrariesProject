7e624991072f60736182431e64c1d71ea770d8cb
IvanVidelaR <videlaivan10@gmail.com>
2024-04-10 15:54:07 -0300

    Forth Project React freeCodeCamp react-calculator fixed some bugs FINISHED

--- React/FreeCodeCamp/04_ReactCalculator/javascript-calculator-project/src/App.jsx
+++ React/FreeCodeCamp/04_ReactCalculator/javascript-calculator-project/src/App.jsx
@@ -14,7 +14,13 @@ function App() {
 	}
 
 	function calculate() {
-		setDisplay(eval(formula));		
+		const resultado = eval(formula);
+		if(!resultado) {
+			setDisplay("Resultado indefinido");
+		}
+		else {
+			setDisplay(eval(formula));	
+		}
 		setCalculated(true);
 	}
 
@@ -36,7 +42,11 @@ function App() {
 
 			const lastCharFormula = formula[formula.length - 1]
 			const lastLastCharFormula = formula[formula.length - 2]
-			if(!calculated) 
+			if(display == "Resultado indefinido") {
+				setDisplay("0");
+				setFormula("");
+			}
+			else if(!calculated) 
 			{
 				if(isOperator(lastCharFormula))
 				{
@@ -66,7 +76,7 @@ function App() {
 		} 
 		else if(input == "equals") 
 		{
-			if(formula && !calculated) {
+			if(formula) {
 				calculate()
 			}
 		} 
@@ -91,26 +101,32 @@ function App() {
 		} 
 		else if(input == "0") 
 		{
-			if(display == "0" && !formula)
-			{
-				setDisplay("0");
-				setFormula("0");
+			if(!calculated) {
+				if(display == "0" && !formula)
+				{
+					setDisplay("0");
+					setFormula("0");
+				}
+				else if(formula[formula.length - 1] == "0" && (formula.length == 1 || isOperator(formula[formula.length - 2]))) 
+				{
+					setFormula(prevFormula => prevFormula.slice(0, -1) + input);
+					setDisplay(prevDisplay => prevDisplay.slice(0, -1) + input);
+				}
+				else if(isOperator(display)) 
+				{
+					setFormula(prevFormula => prevFormula + input);
+					setDisplay(prevDisplay => prevDisplay.slice(0, -1) + input);
+				}
+				else 
+				{
+					setFormula(prevFormula => prevFormula + input);
+					setDisplay(prevDisplay => prevDisplay + input);
+				}
 			}
-			else if(formula[formula.length - 1] == "0" && (formula.length == 1 || isOperator(formula[formula.length - 2]))) 
-			{
-				setFormula(prevFormula => prevFormula.slice(0, -1) + input);
-				setDisplay(prevDisplay => prevDisplay.slice(0, -1) + input);
-			}
-			else if(isOperator(display)) 
-			{
-				setFormula(prevFormula => prevFormula + input);
-				setDisplay(prevDisplay => prevDisplay.slice(0, -1) + input);
-			}
-			else 
-			{
-				setFormula(prevFormula => prevFormula + input);
-				setDisplay(prevDisplay => prevDisplay + input);
-			}
+			else {
+				setFormula(input);
+				setDisplay(input);
+			}			
 		}
 		else 
 		{
