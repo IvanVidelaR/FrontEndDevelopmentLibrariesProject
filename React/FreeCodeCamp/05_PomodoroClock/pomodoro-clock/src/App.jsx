c362c196a89aec5c197d0d9e17e3d1d35c313326
IvanVidelaR <videlaivan10@gmail.com>
2024-04-13 22:56:22 -0300

    Subo estructura pomodoro-clock

--- React/FreeCodeCamp/05_PomodoroClock/pomodoro-clock/src/App.jsx
+++ React/FreeCodeCamp/05_PomodoroClock/pomodoro-clock/src/App.jsx
@@ -1,35 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [breakLength, setBreakLength] = useState(5);
+	const [sessionLength, setSessionLength] = useState(25);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="container">
+			<h1 className="pomodoro-title">Pomodoro Clock</h1>
+			<div className="session-length">
+				<h2 id="session-label">
+					Session Length
+				</h2>
+				<button id="session-increment">
+					<i class="fa-solid fa-circle-up"></i>
+				</button>
+				<p id="session-length">
+					{sessionLength}
+				</p>
+				<button id="session-decrement">
+					<i class="fa-solid fa-circle-down"></i>
+				</button>
+			</div>
+			<div className="break-length">
+				<h2 id="break-label">
+					Break Length
+				</h2>
+				<button id="break-increment">
+					<i class="fa-solid fa-circle-up"></i>
+				</button>
+				<p id="break-length">
+					{breakLength}
+				</p>
+				<button id="break-decrement">
+					<i class="fa-solid fa-circle-down"></i>
+				</button>
+			</div>
+			<div className="timer">
+				<h2 id="timer-label">
+					Pomodoro
+				</h2>
+				<p id="time-left">
+					25:00
+				</p>
+			</div>
+			<div className="timer-control">
+				<button id="start_stop">
+					start
+				</button>
+				<button id="reset">
+					<i class="fa-solid fa-rotate-right"></i>
+				</button>
+			</div>
+			<p className="credits">IvanVidelaR</p>
+		</div>
+	)
 }
 
 export default App
